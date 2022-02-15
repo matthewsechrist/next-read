13ea317a393a20ecc7b7b0750149913bd31759aa
MattSechrist <matthew.sechrist@gmail.com>
2022-02-15 07:49:22 -0500

    Update books.js

--- books.js
+++ books.js
@@ -26,7 +26,7 @@ async function books() {
           for (var j = 0; j < isbn.length; j++) {
             if (isbn[j].type === "ISBN_10" && obj.items[i].volumeInfo.description) {
 
-				var src = obj.items[i].volumeInfo.imageLinks.thumbnail,
+				var src = obj.items[i].volumeInfo.imageLinks.thumbnail.replace('http://','https://');
 				img = document.createElement("img"),
 				div = document.createElement("div");
 
@@ -37,7 +37,8 @@ async function books() {
 
 			  img.src = src;
 			  document.getElementById("content").appendChild(div);
-			  document.getElementById(isbn[j].identifier.toString()).append(img);	
+			  document.getElementById(isbn[j].identifier.toString()).append(img);
+			  //document.getElementById(isbn[j].identifier.toString()).append(obj.items[i].volumeInfo.description.industryIdentifiers.categories.toString());	
 	
             }
           }
@@ -65,9 +66,10 @@ async function get_stuff(isbn) {
     .then((response2) => response2.text())
     .then((body) => {
       var obj = JSON.parse(body);
-      div.append("ISBN:" + isbn.toString() + JSON.stringify(obj.Payload));
-      document.getElementById(isbn.toString()).append("ISBN:" + isbn.toString() + JSON.stringify(obj.Payload));
-	  console.log(document.getElementById(isbn.toString()) === true)
+	//   console.log(obj);
+	//   console.log(body);
+      div.append("ISBN:" + isbn.toString());
+      document.getElementById(isbn.toString()).append("ISBN:" + isbn.toString()+JSON.stringify(obj));
     })
     .catch(function (error) {
       console.log(error);
@@ -81,29 +83,5 @@ input.addEventListener("keyup", function (event) {
     books();
   }
 });
-
-//function clicked() {
-//  //var parent = this.parentNode;
-//  var div = document.createElement("div");
-//  var text = document.createTextNode("texxxt");
-//  div.appendChild(text);
-//  document.getElementById("content").appendChild(div);
-//  const response2 = await fetch(
-//    "https://uk4tq4pat9.execute-api.us-east-1.amazonaws.com/Prod/execution",
-//    {
-//      method: "POST",
-//      headers: { "Content-Type": "application/json" }, // this line is important, if this content-type is not set it wont work
-//      body: '{"book":+"1469664976"}',
-//    }
-//  )
-//    .then((response2) => response2.text())
-//    .then((body) => {
-//
-//  	var obj = JSON.parse(body);
-//  	console.log(obj);
-//  	return obj;
-//    }
-//  )
-//}
 
 books();
