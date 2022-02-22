50785d83583ac11279830ca52118373498f5068d
MattSechrist <matthew.sechrist@gmail.com>
2022-02-21 22:16:34 -0500

    clean up nextread folder

--- books.js
+++ books.js
@@ -1,68 +1,75 @@
-var authors = [],
-  isbn,
-  isbns = [],
-  newAuthor = [],
-  newAuthors = [],
-  search_item;
+var authors           = [],
+    isbns             = [],
+    flattened_authors = [],
+    filtered_authors  = [],
+    isbn              = "",
+    searched_author   = "";
 
-async function getBooks() {
-  search_item = document.getElementById("search").value.replaceAll(" ", "+");
+async function searchAuthor() {
+  // Set searched_author variable to searched valued
+  searched_author = document.getElementById("search").value;
 
+  authors         = [],
+  isbns           = []  
+
+  // Null out HTML elements for book content and authors
   document.getElementById("content").innerHTML = "";
   document.getElementById("authors").innerHTML = "";
 
-  if (search_item) {
+  // If a valid author is searched, pull the first 10 books for the searched author
+  if (searched_author) {
     document.createElement("authors");
 
     let books = await fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=inauthor:${" +
-        search_item +
-        "}&maxResults=10"
+      "https://www.googleapis.com/books/v1/volumes?q=inauthor:${\"" +
+        searched_author +
+        "\"}&maxResults=10"
     ).then((response) => response.json());
+    
     this.books = books;
 
-    for (i in books.items) {
-      isbn = books.items[i].volumeInfo.industryIdentifiers;
+    // Each book results must have a 10 digint ISBN and a book description. An array of ISBNs is created 
+    // for further processing in the getMentionedAuthors() function
+    for (var book in books.items) {
+      isbn = books.items[book].volumeInfo.industryIdentifiers;
+      
       for (var j = 0; j < isbn.length; j++) {
-        if (
-          isbn[j].type === "ISBN_10" &&
-          books.items[i].volumeInfo.description
-        ) {
-          document
-            .getElementById("content")
-            .append(isbn[j].identifier.toString());
+        if (isbn[j].type === "ISBN_10" && books.items[book].volumeInfo.description) {
+          
+          document.getElementById("content").append(isbn[j].identifier.toString());
           isbns.push(isbn[j].identifier);
 
-          if (books.items[i].volumeInfo.imageLinks)
-            var src = books.items[i].volumeInfo.imageLinks.thumbnail.replace(
-              "http://",
-              "https://"
-            );
-          (img = document.createElement("img")),
-            (div = document.createElement("div"));
+          // Need to change from HTTP to HTTPS for the Google Books image link
+          if (books.items[book].volumeInfo.imageLinks) {
+            var src = books.items[book].volumeInfo.imageLinks.thumbnail.replace("http://","https://");
+          }
+
+          // Set HTML element values
+          img     = document.createElement("img");
+          div     = document.createElement("div");
+          img.id  = isbn[j].identifier;
+          img.src = src;
 
           div.setAttribute("id", isbn[j].identifier.toString());
 
-          img.id = isbn[j].identifier;
-
-          img.src = src;
+          // Add HTML elements
           document.getElementById("content").appendChild(div);
           document.getElementById(isbn[j].identifier.toString()).append(img);
         }
       }
     }
   }
+
+  // Remove any "falsy" ISBNs
   filtered_isbns = isbns.filter(Boolean);
-  console.log(filtered_isbns);
-  for (i in filtered_isbns) {
-    await get_stuff(filtered_isbns[i]);
-  }
+
+  // Pseudo-concurrently retrieve all potential authors mentioned in the book description
+  await Promise.allSettled(filtered_isbns.map(stuff => getMentionedAuthors(stuff)));
+
   addAuthors();
 }
 
-async function get_stuff(isbn) {
-  var parent = this.parentNode;
-  console.log(isbn);
+async function getMentionedAuthors(isbn) {
 
   var div = document.createElement("div");
 
@@ -74,6 +81,7 @@ async function get_stuff(isbn) {
       body: '{"book":"' + isbn + '"}',
     }
   ).then((response) => response.json());
+  
   this.response = response;
 
   authors.push(response);
@@ -84,26 +92,31 @@ async function get_stuff(isbn) {
     .append("ISBN:" + isbn.toString() + JSON.stringify(response));
 }
 
+// Call the searchAuthor() function from the Enter key, also the "Click Me!" button
 var input = document.getElementById("search");
 input.addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    document.getElementById("content").innerHTML = "";
-    document.getElementById("authors").innerHTML = "";
 
-    getBooks();
-  }
-});
-async function addAuthors() {
-  newAuthor = authors.flat();
-  newAuthors = newAuthor.filter(Boolean);
+    searchAuthor();
+  }});
+
+// This function adds the authors to HTML in order of number of books associated by author in descending order,
+// mimicking a sort by relevancy
+function addAuthors() {
+  // Step 1 of 3 - Flatten multiple author arrays into one authors array
+  flattened_authors = authors.flat();
+
+  // Step 2 of 3 - Remove "falsy" authors from the flattened_authors array
+  filtered_authors = flattened_authors.filter(Boolean);
   
-  unique = [...new Map(newAuthors.map((v) => [JSON.stringify(v), v])).values()].sort((a, b) => b[1] - a[1]);
+  // Step 3 of 3 - Final authors author is first converted to a map, the sorted in descending order, then converted back to a correctly ordered array
+  sorted_authors = [...new Map(filtered_authors.map((v) => [JSON.stringify(v), v])).values()].sort((a, b) => b[1] - a[1]);
 
-
-  if (search_item) {
-    document.getElementById("authors").append(JSON.stringify(unique));
+  // Only add the authors HTML div only after an author has been searched
+  if (searched_author) {
+    document.getElementById("authors").append(JSON.stringify(sorted_authors));
   }
 }
 
-getBooks();
+searchAuthor();
