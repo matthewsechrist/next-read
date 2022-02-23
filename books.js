221ad410c7bce86e2caa2c6d6e7945e296369040
MattSechrist <matthew.sechrist@gmail.com>
2022-02-22 20:31:56 -0500

    nextread updates

--- books.js
+++ books.js
@@ -1,16 +1,16 @@
-var authors           = [],
-    isbns             = [],
-    flattened_authors = [],
-    filtered_authors  = [],
-    isbn              = "",
-    searched_author   = "";
+var authors = [],
+  isbns = [],
+  flattened_authors = [],
+  filtered_authors = [],
+  dictionary_authors = {},
+  isbn = "",
+  searched_author = "";
 
 async function searchAuthor() {
   // Set searched_author variable to searched valued
   searched_author = document.getElementById("search").value;
 
-  authors = [],
-  isbns   = []  
+  (authors = []), (isbns = []);
 
   // Null out HTML elements for book content and authors
   document.getElementById("content").innerHTML = "";
@@ -21,33 +21,40 @@ async function searchAuthor() {
     document.createElement("authors");
 
     let books = await fetch(
-      "https://www.googleapis.com/books/v1/volumes?q=inauthor:${\"" +
+      'https://www.googleapis.com/books/v1/volumes?q=inauthor:${"' +
         searched_author +
-        "\"}&maxResults=10"
+        '"}&maxResults=10'
     ).then((response) => response.json());
-    
+
     this.books = books;
 
-    // Each book results must have a 10 digint ISBN and a book description. An array of ISBNs is created 
+    // Each book results must have a 10 digint ISBN and a book description. An array of ISBNs is created
     // for further processing in the getMentionedAuthors() function
     for (var book in books.items) {
       isbn = books.items[book].volumeInfo.industryIdentifiers;
-      
+
       for (var j = 0; j < isbn.length; j++) {
-        if (isbn[j].type === "ISBN_10" && books.items[book].volumeInfo.description) {
-          
-          document.getElementById("content").append(isbn[j].identifier.toString());
+        if (
+          isbn[j].type === "ISBN_10" &&
+          books.items[book].volumeInfo.description
+        ) {
+          document
+            .getElementById("content")
+            .append(isbn[j].identifier.toString());
           isbns.push(isbn[j].identifier);
 
           // Need to change from HTTP to HTTPS for the Google Books image link
           if (books.items[book].volumeInfo.imageLinks) {
-            var src = books.items[book].volumeInfo.imageLinks.thumbnail.replace("http://","https://");
+            var src = books.items[book].volumeInfo.imageLinks.thumbnail.replace(
+              "http://",
+              "https://"
+            );
           }
 
           // Set HTML element values
-          img     = document.createElement("img");
-          div     = document.createElement("div");
-          img.id  = isbn[j].identifier;
+          img = document.createElement("img");
+          div = document.createElement("div");
+          img.id = isbn[j].identifier;
           img.src = src;
 
           div.setAttribute("id", isbn[j].identifier.toString());
@@ -64,13 +71,14 @@ async function searchAuthor() {
   filtered_isbns = isbns.filter(Boolean);
 
   // Pseudo-concurrently retrieve all potential authors mentioned in the book description
-  await Promise.allSettled(filtered_isbns.map(stuff => getMentionedAuthors(stuff)));
+  await Promise.allSettled(
+    filtered_isbns.map((stuff) => getMentionedAuthors(stuff))
+  );
 
   addAuthors();
 }
 
 async function getMentionedAuthors(isbn) {
-
   var div = document.createElement("div");
 
   const response = await fetch(
@@ -81,7 +89,7 @@ async function getMentionedAuthors(isbn) {
       body: '{"book":"' + isbn + '"}',
     }
   ).then((response) => response.json());
-  
+
   this.response = response;
 
   authors.push(response);
@@ -99,7 +107,8 @@ input.addEventListener("keyup", function (event) {
     event.preventDefault();
 
     searchAuthor();
-  }});
+  }
+});
 
 // This function adds the authors to HTML in order of number of books associated by author in descending order,
 // mimicking a sort by relevancy
@@ -109,16 +118,30 @@ function addAuthors() {
 
   // Step 2 of 3 - Remove "falsy" authors from the flattened_authors array
   filtered_authors = flattened_authors.filter(Boolean);
-  
   // Need to remove duplicates by author name
 
-  // Step 3 of 3 - Final authors author is first converted to a map, the sorted in descending order, then converted back to a correctly ordered array
-  sorted_authors = [...new Map(filtered_authors.map((v) => [JSON.stringify(v), v])).values()].sort((a, b) => b[1] - a[1]);
+  // Step 3 of 3 - Create a dictionary of authors which does not allow duplicates of authors,
+  // and this maps the JSON returned in the format author:books
+  dictionary_authors = Object.assign(
+    {},
+    ...filtered_authors.map((x) => ({ [x.author]: x.books }))
+  );
+
+  var ordered_authors = Object.keys(dictionary_authors).map(function (key) {
+    return [key, dictionary_authors[key]];
+  });
+
+  // Sorts the ordered_authors array in descending order, mimicking relevancy of authors
+  ordered_authors.sort(function (first, second) {
+    return second[1] - first[1];
+  });
 
   // Only add the authors HTML div only after an author has been searched
   if (searched_author) {
-    document.getElementById("authors").append(JSON.stringify(sorted_authors));
-  }
+    //  document.getElementById("authors").append(JSON.stringify(sorted_authors));
+    for (i in ordered_authors){
+    document.getElementById("authors").append(ordered_authors[i][0] + ordered_authors[i][1]);
+  }}
 }
 
 searchAuthor();
--- lambda/next_read_find_authors.py
+++ lambda/next_read_find_authors.py
@@ -4,8 +4,6 @@ import re
 from urllib.request import urlopen
 
 def next_read_find_authors(event, context):
-    #return event['Payload']
-    #print(event['value']+"gg")
     potential_author = event['value']
     
     book_api    = "https://www.googleapis.com/books/v1/volumes?q=author:"+'"'+potential_author+'"'
@@ -14,22 +12,21 @@ def next_read_find_authors(event, context):
     book_data   =json.load(resp)
     
     authors = []
-    mentioned_authors = []
     
-    
+    # Verify author has any books attributed to them
     if (book_data['totalItems'] > 0):
-        print(book_data['totalItems'])
 
-        
+        # Iterate over all books returned
         for index,book in enumerate(book_data['items']):
+            # Verify returned book has authors listed
             if ("authors" not in book_data['items'][index]['volumeInfo']):
                 return None
+            
+            # Assign authors JSON to authors variable    
             authors = book_data['items'][index]['volumeInfo']['authors']
+            
+            # Iterate over all authors
             for author_index,author in enumerate(authors):
-                if potential_author.replace('+',' ').lower() == author.lower():
-                    print(potential_author.replace('+',' '))
-                    print(author)
-                    print(potential_author.replace('+',' ').lower() == author.lower())
-                    mentioned_authors.append({"author":potential_author})
-                
-                    return [potential_author,book_data['totalItems']]
+                # Return the potential author if 
+                if (potential_author.replace('+',' ').lower() == author.lower()):
+                    return {"author":author,"books":book_data['totalItems']}     
